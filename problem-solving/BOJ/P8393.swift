73a1866916a73a8357977813ee2aa4dcf993ea5b
Geonhee Son <ryan.son1002@gmail.com>
2022-11-15 23:03:48 +0900

    solved: Bronze 5 - 8393 합

---
+++ problem-solving/BOJ/P8393.swift
@@ -0,0 +1,13 @@
+//
+//  P8393.swift
+//  problem-solving
+//
+//  Created by Geonhee on 2022/11/14.
+//
+
+/// [브론즈5 합](https://www.acmicpc.net/problem/8393)
+func p8393() {
+    let n = Int(readLine()!)!
+    let sum = (n * (n + 1)) / 2
+    print("\((sum))")
+}
