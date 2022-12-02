65a293f17a7472f7e8c3c7efabce653bcd319a54
Geonhee Son <ryan.son1002@gmail.com>
2022-12-02 22:28:14 +0900

    solved: Bronze 5 - 11654 아스키 코드

---
+++ problem-solving/BOJ/Solved/P11654.swift
@@ -0,0 +1,12 @@
+//
+//  P11654.swift
+//  problem-solving
+//
+//  Created by Geonhee on 2022/12/02.
+//
+
+/// [브론즈5 아스키 코드](https://www.acmicpc.net/problem/11654)
+func p11654() {
+    let character = Character(readLine()!)
+    print(character.asciiValue!)
+}
