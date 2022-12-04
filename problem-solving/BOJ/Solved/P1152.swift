3693e523e7a95a796f8cff7cafd4fdbdd8af8ad2
Geonhee Son <ryan.son1002@gmail.com>
2022-12-04 10:36:35 +0900

    solved: Bronze 2 - 1152 단어의 개수

---
+++ problem-solving/BOJ/Solved/P1152.swift
@@ -0,0 +1,13 @@
+//
+//  P1152.swift
+//  problem-solving
+//
+//  Created by Geonhee on 2022/12/04.
+//
+
+/// [브론즈2 단어의 개수](https://www.acmicpc.net/problem/1152)
+func p1152() {
+    let sentence = readLine()!
+    let words = sentence.split(separator: " ")
+    print(words.count)
+}
